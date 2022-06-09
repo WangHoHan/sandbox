654552bc3a95c7914719b9d538766e5e950b2ae1
Tomasz Adamczyk <tpadamczyk7@gmail.com>
2022-06-09 15:20:13 +0200

    build: connect graphql to contentful (#3)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import { CONTENTFUL_GRAPHQL_BASE_URI } from "../constants/constants";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+export const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: `${CONTENTFUL_GRAPHQL_BASE_URI}${process.env.CONTENTFUL_SPACE_ID}`,
+  headers: {
+    Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_KEY}`,
+  },
+});
 
-export default MyApp
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+};
+
+export default MyApp;
