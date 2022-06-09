e52680ce331dd657137288b4ebec9399f4162674
Tomasz Adamczyk <tpadamczyk7@gmail.com>
2022-06-09 16:15:37 +0200

    build: add emotion and setup emotion theme (#4)
    * build: add emotion and setup emotion theme
    
    * fix: move styles to index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import { client } from "./_app";
 import { POKEMONS_QUERY } from "../graphql/queries/pokemons";
 
-export async function getStaticProps() {
+export const getStaticProps = async () => {
   const { data } = await client.query({ query: POKEMONS_QUERY });
 
   return {
@@ -10,16 +10,71 @@ export async function getStaticProps() {
       data,
     },
   };
-}
+};
 
 const Home: NextPage = (props) => {
   return (
-    <>
-      {props["data"].pokemonCollection.items.map((pokemon) => {
-        return <div key={pokemon.index}>{pokemon.name}</div>;
-      })}
-    </>
+    <HomeStyled>
+      <Pokemons>
+        {props["data"].pokemonCollection.items.map((pokemon) => {
+          return (
+            <Pokemon key={pokemon.index}>
+              <Name>{pokemon.name}</Name>
+              <Generation></Generation>
+              <Types>
+                {pokemon.types.map((type) => (
+                  <Type key={type}>{type}</Type>
+                ))}
+              </Types>
+              <Species>{pokemon.species}</Species>
+              <Abilities>
+                {pokemon.abilities.map((ability) => {
+                  <Ability key={ability}>{ability}</Ability>;
+                })}
+              </Abilities>
+              <Index>{pokemon.index}</Index>
+              <EvolvesFrom>{pokemon.evolvesFrom}</EvolvesFrom>
+              <EvolvesInto>{pokemon.evolvesInto}</EvolvesInto>
+              <Weight>{pokemon.weight}</Weight>
+              <Height>{pokemon.height}</Height>
+            </Pokemon>
+          );
+        })}
+      </Pokemons>
+    </HomeStyled>
   );
 };
+
+import styled from "@emotion/styled";
+
+const HomeStyled = styled.div``;
+
+const Pokemons = styled.div``;
+
+const Pokemon = styled.div``;
+
+const Name = styled.span``;
+
+const Generation = styled.span``;
+
+const Types = styled.div``;
+
+const Type = styled.span``;
+
+const Species = styled.span``;
+
+const Abilities = styled.div``;
+
+const Ability = styled.span``;
+
+const Index = styled.span``;
+
+const EvolvesFrom = styled.span``;
+
+const EvolvesInto = styled.span``;
+
+const Weight = styled.span``;
+
+const Height = styled.span``;
 
 export default Home;
