db0fc20f46144ab8d3fcb636728c59a37be55335
Tomasz Adamczyk <tpadamczyk7@gmail.com>
2022-06-10 16:36:38 +0200

    feat: add language support for code (#7)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import type { AppProps } from "next/app";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import { CONTENTFUL_GRAPHQL_BASE_URI } from "../constants/constants";
+import { useRouter } from "next/router";
+import { IntlProvider } from "react-intl";
 import { Global, ThemeProvider } from "@emotion/react";
 import { global } from "../styles/globals";
+import en_US from "../lang/en-US.json";
+import pl from "../lang/pl.json";
 
 export const client = new ApolloClient({
   cache: new InMemoryCache(),
@@ -12,12 +16,20 @@ export const client = new ApolloClient({
   },
 });
 
+const messages = {
+  "en-US": en_US,
+  pl,
+};
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const { locale } = useRouter();
   return (
     <ApolloProvider client={client}>
       <ThemeProvider theme={{}}>
-        <Global styles={global} />
-        <Component {...pageProps} />
+        <IntlProvider locale={locale} messages={messages[locale]}>
+          <Global styles={global} />
+          <Component {...pageProps} />
+        </IntlProvider>
       </ThemeProvider>
     </ApolloProvider>
   );
