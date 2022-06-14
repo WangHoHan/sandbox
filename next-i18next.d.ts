0f4407d80486498d548b5fd9f0cc28d4cdb4bc5f
Tomasz Adamczyk <tpadamczyk7@gmail.com>
2022-06-14 12:57:51 +0200

    refactor: split translation files (#14)
    * refactor: move public/locales into /translations directory
    
    * refactor: split translation files into multiple ones

--- next-i18next.d.ts
+++ next-i18next.d.ts
@@ -1,13 +1,11 @@
 import 'next-i18next';
-import ns1 from './public/locales/en-US/common.json';
-import ns2 from './public/locales/pl/common.json';
-
+import common from './translations/en-US/common.json';
+import hello from './translations/en-US/hello.json';
 declare module 'react-i18next' {
   interface CustomTypeOptions {
-    defaultNS: 'ns1';
     resources: {
-      ns1: typeof ns1;
-      ns2: typeof ns2;
+      common: typeof common;
+      hello: typeof hello;
     };
   }
 }
